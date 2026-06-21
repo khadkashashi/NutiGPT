8c7f6ece88f64d03732d18ea27d2c6809297b83c
shashi khadka <shashikhadka84@gmail.com>
2026-06-21 18:04:18 +0545

    initial commit

---
+++ meal/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class MealPlan(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_mealplan')
+    tittle = models.CharField()
+    start_date= models.DateField()
+    end_date=models.DateField()
+    generated_by_ai= models.BooleanField(default=False)
+    created_at =models.DateField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+    
+
