e4fccf7a19cef14442e59c3c215d6136fc5d2c97
shashi khadka <shashikhadka84@gmail.com>
2026-06-24 17:36:41 +0545

    add pagination

--- meal/urls.py
+++ meal/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from meal.views import MealView
+
+from .views import MealCreateView, MealDeleteView, MealUpdateView, MealView
+
 urlpatterns = [
-    path('',MealView.as_view(),name="meal-view"),
+    path('', MealView.as_view(), name="meal-list"),
+    path('create/', MealCreateView.as_view(), name="meal-create"),
+    path('update/<int:pk>/', MealUpdateView.as_view(), name="meal-update"),
+    path('delete/<int:pk>/', MealDeleteView.as_view(), name="meal-delete"),
 ]
--- meal/views.py
+++ meal/views.py
@@ -20,7 +20,39 @@ class MealUpdateView(UpdateView):
     template_name = 'meal/create.html'
     form_class = MealForm
     success_url = '/meal'
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from meals.models import Meal
+from meals.forms import MealForm
+# Create your views here.
+
+
+class MealView(ListView):
+    model = Meal
+    paginate_by = 10
+    template_name = 'meal/index.html'
+    context_object_name = 'meal'
+
+
+class MealCreateView(CreateView):
+    model=Meal
+    template_name = 'meal/create.html'
+    form_class = MealForm
+    success_url = '/meal'
+
+
+class MealUpdateView(UpdateView):
+    model=Meal
+    template_name = 'meal/update.html'
+    form_class = MealForm
+    success_url = '/meal'
+
+
+class MealDeleteView(DeleteView):
+    model=Meal
+    success_url = '/meal'
 class MealDeleteView(DeleteView):   
     model = Meal
     success_url = '/meal'
